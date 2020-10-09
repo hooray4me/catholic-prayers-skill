410f343f4fe23770a0726896cda75efc0a7035ad
Rob Hedrick <rob@hedrickholdings.com>
2020-10-09 14:06:51 -0500

    Initial testing

--- __init__.py
+++ __init__.py
@@ -1,5 +1,12 @@
+import io
+import requests
+import json
 from mycroft import MycroftSkill, intent_file_handler
 
+def apiResponse(ipAddress,cmd):
+    uri="http://" + str(ipAddress) + "/YamahaExtendedControl/v1/" + str(cmd)
+    response = requests.get(uri)
+    return response
 
 class CatholicPrayers(MycroftSkill):
     def __init__(self):
@@ -7,7 +14,12 @@ class CatholicPrayers(MycroftSkill):
 
     @intent_file_handler('prayers.catholic.intent')
     def handle_prayers_catholic(self, message):
-        self.speak_dialog('prayers.catholic')
+		ipAddress = self.settings["yamaha_ip"]
+		r = apiResponse(ipAddress,"main/getStatus")
+		t = str(r.json().get("power"))
+		print t
+        #self.speak_dialog('prayers.catholic')
+		self.speak_dialog('prayers.catholic', {"status": t})
 
 
 def create_skill():
